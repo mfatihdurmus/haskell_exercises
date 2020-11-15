2a2a7f956039c528811ca1241abd831911166c12
mfatihdurmus <fatihdurmus@live.com>
2020-11-15 03:48:40 +0300

    system.random example

---
+++ guessrandnumber.hs
@@ -0,0 +1,19 @@
+import System.IO ()
+import System.Random ( getStdGen, newStdGen, Random(randomR), StdGen )
+import Control.Monad (unless)
+
+main :: IO ()
+main = do
+    putStrLn "Can you guess the number?"
+    
+    g <- getStdGen
+    let (randomNumber, _) = randomR (1, 10) g :: (Int, StdGen)
+    
+    numberString <- getLine
+    unless (null numberString) $ do
+        let number = read numberString
+        if number == randomNumber 
+            then putStrLn "Correct!"
+            else putStrLn $ "Sorry, it was " ++ show randomNumber
+        newStdGen
+        main
